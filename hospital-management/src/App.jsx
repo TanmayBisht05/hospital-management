de4f6db0d74c2639436afbf39c700107f0a84ede
Your Name <you@example.com>
2024-07-31 23:31:16 +0530

    about page

--- hospital-management/src/App.jsx
+++ hospital-management/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Section1 from './components/section1/Section1.jsx'
 import Navbar from './components/navbar/navbar.jsx'
+import About from './components/about/About.jsx'
 import Footer from './components/footer.jsx'
+
+
+
 function App() {
 
   return (
     <>
       <Navbar />
       <Section1 />
+      <About />
       <Footer />
     </>
   )
