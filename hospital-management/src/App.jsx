8fdb0b47f3145c6414645fe73da392d506e8b32d
Eshaan Sodhi <eshaansodhi@Eshaans-MacBook-Pro.local>
2024-07-31 22:36:40 +0530

    router

--- hospital-management/src/App.jsx
+++ hospital-management/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Section1 from './components/section1/Section1.jsx'
 import Navbar from './components/navbar/navbar.jsx'
 import Footer from './components/footer.jsx'
+import Contact from './pages/contact.jsx'
+
 function App() {
 
   return (
     <>
-      <Navbar />
       <Section1 />
-      <Footer />
     </>
   )
 }
