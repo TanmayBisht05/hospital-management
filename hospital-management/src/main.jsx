144261198d115153b1aee168ed2a600514e599b5
Ashish Bazad <ashishbazad@Ashishs-MacBook-Air.local>
2024-07-31 22:53:36 +0530

    fake

--- hospital-management/src/main.jsx
+++ hospital-management/src/main.jsx
@@ -2,20 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Contact from './pages/contact.jsx'
-import Navbar from './components/navbar/navbar.jsx'
-import Footer from './components/footer.jsx'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
+    <App />
   </React.StrictMode>,
 )
