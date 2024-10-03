58bdf0084f2117f69f73e454da6abc33f503afc6
Shikhar Srivastav <shikhars2003@gmail.com>
2024-10-03 12:04:29 +0530

    Added doctors api endpoint support

--- hospital-management/src/components/doctorTiles/DoctorTiles.jsx
+++ hospital-management/src/components/doctorTiles/DoctorTiles.jsx
@@ -8,16 +8,23 @@ const DoctorTiles = ({params}) => {
         <div className="tile_container">
             <div className="tile">
                 <div className="tile_image">
-                    <img src={params.img} alt="doctor" />
+                    <img src="#" alt="doctor" />
                 </div>
                 <div className='tile_details'>
                     <center>
-                    <span className='tile_name'>{params.name}</span><br />
-                    <span className='tile_spec'>{params.spec}</span>
+                    <span className='tile_name'>{params.firstName} {params.lastName}</span><br />
+                    <span className='tile_spec'>{params.specialization}</span>
                     </center>
                 </div>
                 <div className='tile_desc'>
-                    <p>{params.desc}</p>
+                    <p>{params.phone}</p>
+                    <p>{params.mail}</p>
+                    <p>{params.education}</p>
+                    <p>{params.post}</p>
+                    <p>{params.department}</p>
+                    <p>{params.specialization}</p>
+
+
                     {/* <p>Dr. Ashish is a Cardiologist with 10 years of experience.</p>
                     <p>He is available at XYZ hospital on Monday, Wednesday and Friday.</p>
                     <p>He is available for online consultation.</p> */}
