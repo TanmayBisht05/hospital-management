7574b50d0e0c612b40b1c170beece2bfee724419
Ashish Bazad <ashishbazad@Ashishs-MacBook-Air.local>
2024-10-24 12:48:07 +0530

    appointment cards

--- hospital-management/src/pages/patient/Pdashboard.jsx
+++ hospital-management/src/pages/patient/Pdashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar.jsx'
 import './pdashboard.css'
 import AuthContext from '../../AuthContext.jsx'
+import App_cards from '../../components/app_cards/App_cards.jsx'
+
 const pdashboard = () => {
   const { pdashboardState } = React.useContext(AuthContext);
 
@@ -9,11 +11,38 @@ const pdashboard = () => {
     <div className='pdashboard'>
       <Sidebar />
       <div className="main-content">
-        {pdashboardState === 0 && <div><h1 className="dashboard-header">Profile</h1></div>}
-        {pdashboardState === 1 && <div><h1 className="dashboard-header">Appointments</h1></div>}
-        {pdashboardState === 2 && <div><h1 className="dashboard-header">New Appointment</h1></div>}
-        {pdashboardState === 3 && <div><h1 className="dashboard-header">Pending Bills</h1></div>}
-        {pdashboardState === 4 && <div><h1 className="dashboard-header">History</h1></div>}
+        {pdashboardState === 0 && <>
+          <h1 className="dashboard-header">Profile</h1>
+        </>}
+        {pdashboardState === 1 && <>
+          <h1 className="dashboard-header">Appointments</h1>
+          <div className="appointments">
+            <h2>Upcoming Appointments</h2>
+            <div className="appointment_cards">
+              <App_cards />
+              <App_cards />
+              <App_cards />
+            </div>
+          </div>
+          <div className="appointments">
+            <h2>Previous Appointments</h2>
+            <div className="appointment_cards">
+              <App_cards />
+              <App_cards />
+              <App_cards />
+            </div>
+          </div>
+        </>}
+        {pdashboardState === 2 && <>
+          <h1 className="dashboard-header">New Appointment</h1>
+        </>}
+        {pdashboardState === 3 && <>
+          <h1 className="dashboard-header">Pending Bills</h1>
+        </>}
+        
+        {pdashboardState === 4 && <>
+          <h1 className="dashboard-header">History</h1>
+        </>}
       </div>
     </div>
   )
