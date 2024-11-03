3d38a73a40d4b0cfd22eccf5a772fc4f718a1bd8
Shikhar Srivastav <73072934+StoneCoder123@users.noreply.github.com>
2024-11-03 12:23:02 +0530

    Update App.jsx

--- hospital-management/src/App.jsx
+++ hospital-management/src/App.jsx
@@ -7,12 +7,9 @@ import About from './components/about/About.jsx'
 import Pdashboard from './pages/patient/Pdashboard.jsx'
 import { AuthProvider } from './AuthContext'
 import Medicines from './components/medicines/medicine.jsx'
-
-import Login from './pages/login/login.jsx'
-
 import Surgery from './components/surgery/surgery.jsx';
 import TreatmentProcedure from './components/treatmentProcedure/treatmentProcedure.jsx';
-
+import PatientRegistrationForm from './components/patientReg.jsx'
 function App() {
   return (
     <AuthProvider>
@@ -23,15 +20,15 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/doctors" element={<Doctor />} />
           <Route path="/about" element={<About />} />
-          <Route path = "/patients" element = {<Pdashboard />} />
-          <Route path = "/login" element = {<Login />} />
+          <Route path="/register" element={<PatientRegistrationForm />} />
+        <Route path = "/patients" element = {<Pdashboard />} />
           <Route path="/medicines" element={<Medicines />} />
           <Route path="/surgery/:doctorId" element={<Surgery />} /> {/* Updated route */}
           <Route path="/treatmentProcedure" element={<TreatmentProcedure />} />
           <Route path="/treatmentProcedure/:doctorId" element={<TreatmentProcedure />} />
-      </Routes>
-     </BrowserRouter>
-    </AuthProvider>
+        </Routes>
+      </BrowserRouter>
+      </AuthProvider>
   );
 }
 
