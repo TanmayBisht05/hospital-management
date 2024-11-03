da217530aab9bb9415e31528636db062cdeb91f9
Ashish Bazad <ashish.student.cse22@iitbhu.ac.in>
2024-11-03 22:58:37 +0530

    bla bla

--- hospital-management/src/pages/signup/Signup.jsx
+++ hospital-management/src/pages/signup/Signup.jsx
@@ -3,22 +3,20 @@ import AuthContext from '../../AuthContext';
 import Navbar from '../../components/navbar/navbar';
 import Fake from '../../utility/Fake';
 import './signup.css';
+import { useNavigate } from 'react-router-dom';
 const Signup = () => {
     let {signup} = useContext(AuthContext);
-
+    const navigate = useNavigate();
     const handleSignup = async (e) => {
         e.preventDefault();
         const data = {
             firstName: document.getElementById('firstName').value,
             lastName: document.getElementById('lastName').value,
+            address: document.getElementById('address').value,
+            NTK: document.getElementById('NTK').value,
             dob: document.getElementById('dob').value,
-            education: document.getElementById('education').value,
             gender : document.getElementById('gender').value,
-            phone: document.getElementById('phone').value,
             email: document.getElementById('email').value,
-            post: document.getElementById('post').value,
-            department: document.getElementById('department').value,
-            specialization: document.getElementById('specialization').value,
             password: document.getElementById('password').value
         }
         if(await signup(data, "patients")) {
@@ -35,13 +33,6 @@ const Signup = () => {
         <div className="login_div">
             <h1>Signup</h1>
             <form className='login_form' onSubmit={handleSignup}>
-                {/* <div className="login_div">
-                    <label htmlFor="userType" className="login_label">Who are you? : </label>
-                    <select className='login_select' id='userType'>
-                        <option value="patients">Patient</option>
-                        <option value="doctor">Doctor</option>
-                    </select>
-                </div> */}
                 <div className="login_div">
                     <label htmlFor="firstName" className="login_label">First Name : </label>
                     <input className='login_input' id='firstName' type="text" placeholder="First Name" />
@@ -55,8 +46,12 @@ const Signup = () => {
                     <input className='login_input' id='dob' type="date" placeholder="Date of Birth" />
                 </div>
                 <div className="login_div">
-                    <label htmlFor="education" className="login_label">Education : </label>
-                    <input className='login_input' id='education' type="text" placeholder="Education" />
+                    <label htmlFor="address" className="login_label">Address : </label>
+                    <input type="text" className="login_input" placeholder='Address' id='address' />
+                </div>
+                <div className="login_div">
+                    <label htmlFor="NTK" className="login_label">NTK : </label>
+                    <input type="text" className="login_input" placeholder='NTK' id='NTK' />
                 </div>
                 <div className="login_div">
                     <label htmlFor="gender" className="login_label">Gender : </label>
@@ -66,24 +61,8 @@ const Signup = () => {
                     </select>
                 </div>
                 <div className="login_div">
-                    <label htmlFor="phone" className="login_label">Phone : </label>
-                    <input className='login_input' id = 'phone' type = 'text' placeholder = 'Phone'/>
-                </div>
-                <div className="login_div">
                     <label htmlFor="email" className="login_label">Email : </label>
                     <input className='login_input' id='email' type="text" placeholder="Email" />
-                </div>
-                <div className="login_div">
-                    <label htmlFor="post" className="login_label">Post : </label>
-                    <input className='login_input' id = 'post' type = 'text' placeholder = 'Post'/>
-                </div>
-                <div className="login_div">
-                    <label htmlFor="department" className="login_label">Department : </label>
-                    <input className='login_input' id = 'department' type = 'text' placeholder='Department'/>
-                </div>
-                <div className="login_div">
-                    <label htmlFor="specialization" className="login_label">Specialization : </label>
-                    <input className='login_input' id = 'specialization' type = 'text' placeholder='Specialization'/>
                 </div>
                 <div className="login_div">
                     <label htmlFor="password" className="login_label">Password : </label>
